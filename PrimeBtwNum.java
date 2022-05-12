150d055c918dfa695e863de499435bb3fff58bbe
Minhaj Haider <63444276+minhaj4me@users.noreply.github.com>
2022-05-12 15:02:28 +0530

    Add files via upload

---
+++ PrimeBtwNum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class PrimeBtwNum {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner sc = new Scanner(System.in);
+        int low = sc.nextInt();
+        int high = sc.nextInt();
+        for (int i = low; i <= high; i++) {
+            int x = 1;
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    x++;
+                    break;
+                }
+            }
+            if (x <= 1) {
+                System.out.print(i+" ");
+            }
+            // else
+            // continue;
+        }
+
+    }
+}
