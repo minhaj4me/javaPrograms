2f52d60517d8d59b1d2e4f56f5c018264d4acdea
Minhaj Haider <63444276+minhaj4me@users.noreply.github.com>
2022-05-22 23:40:51 +0530

    Add files via upload

---
+++ ARecursion.java
@@ -0,0 +1,12 @@
+public class ARecursion {
+    public static void main(String[] args) {
+        m1(1);
+    }
+
+    static void m1(int i) {
+        System.out.println(i);
+        if (i == 3)
+            return;
+        m1(i + 1);
+    }
+}
