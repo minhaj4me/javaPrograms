65cbe26c0c43254af317c70fc977e436103fa0c3
Minhaj Haider <63444276+minhaj4me@users.noreply.github.com>
2022-05-25 01:09:53 +0530

    Add files via upload

---
+++ ArrayAtAnIndexesReverse.java
@@ -0,0 +1,24 @@
+public class ArrayAtAnIndexesReverse {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        Reverse(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    // static int[] Reverse(int[] arr) { //this is also true....it is written and
+    // think by me
+    static void Reverse(int[] arr) {
+        int i = 3;
+        int j = 7;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        // return arr;
+    }
+}
