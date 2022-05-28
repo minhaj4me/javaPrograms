4cf4a52e17edea8a3cb35ae86c0874344f1e0c37
Minhaj Haider <63444276+minhaj4me@users.noreply.github.com>
2022-05-29 00:35:32 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,12 @@
+package core;
+
+public class Test1 {
+
+    static {
+        main(null);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
