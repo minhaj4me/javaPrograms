32da73191a2d504a681b401ff6e9b9c380db06dc
minhaj4me <minhajhaider007@gmail.com>
2022-07-27 18:09:39 +0530

    Assignment_16_July

---
+++ Assignment16July/src/cube.java
@@ -0,0 +1,16 @@
+//WAP to find the cube of a given number.
+public class cube {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Please Enter a Number to know its Cube");
+		java.util.Scanner sc=new java.util.Scanner(System.in);
+		double cu=sc.nextDouble();
+		double result=cu*cu*cu;
+		System.out.printf("Square of "+cu+ " is %.2f ",result);
+//		System.out.println("Square of "+cu+" is "+ result);
+				
+
+	}
+
+}
