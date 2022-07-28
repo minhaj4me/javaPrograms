68bc803ec5c8696e4c30f1f10317a67cc0ed9822
minhaj4me <minhajhaider007@gmail.com>
2022-07-29 02:13:42 +0530

    Array_+_diiferent dT Store in an array

--- MultipleDataTypeStoreInArray.java
+++ MultipleDataTypeStoreInArray.java
@@ -1,5 +1,9 @@
 class MultipleDataTypeStoreInArray {
     public static void main(String[] args) {
+        System.out.println("Array Definition :- ");
+        System.out.println(
+                "An Array is an Object consisting of a numbered list of variables. Each is a primitive type or reference\n\n");
+
         System.out.println("if we want to store different type of data type in a single unit in an array");
         System.out.println("then we have to create an array of object ");
         System.out.println("then we can store any kind of object we want in the references in the array");
