c7f64652757074d0f089d2da16727277a9ed8205
minhaj4me <minhajhaider007@gmail.com>
2022-08-01 18:10:52 +0530

    d2

---
+++ CORE JAVA/Fundamental_L1/d2_10/IntegerLiteralEx.java
@@ -0,0 +1,12 @@
+class IntegerLiteralEx{
+    static public void main(String []s){
+        int a=104;
+        System.out.println(a);
+        int b=0150;
+        System.out.println(b);
+        int c=0x68;
+        System.out.println(c);
+        int d=0b1101000;
+        System.out.println(d);
+    }
+}
---
+++ CORE JAVA/Fundamental_L1/d2_10/IntegerLiteralQues.java
@@ -0,0 +1,6 @@
+class IntegerLiteralQues {
+    static public void main(String[] s) {
+        int a = 0x68;
+        System.out.println(a);
+    }
+}
