b04add0bd7e16d6ee4f1f5c2b11c4e621bcc8095
minhaj4me <minhajhaider007@gmail.com>
2022-08-01 18:06:30 +0530

    d1

---
+++ CORE JAVA/Fundamental_L1/d1_9_july/Hello.java
@@ -0,0 +1,9 @@
+class Demo{
+    static public void main(String []minhaj){
+        System.out.printf("Hello World\n");
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
