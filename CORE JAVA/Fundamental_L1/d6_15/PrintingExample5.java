4c37a370423b02902f5dcfa296b11defa7b94d12
minhaj4me <minhajhaider007@gmail.com>
2022-08-14 16:17:11 +0530

    d6

---
+++ CORE JAVA/Fundamental_L1/d6_15/PrintingExample5.java
@@ -0,0 +1,11 @@
+
+public class PrintingExample5 {
+
+	public static void main(String[] args) {
+		System.out.println();//allowed
+		//System.out.print();//error
+		//System.out.printf();//error
+		//System.out.format();//error
+	}
+
+}
