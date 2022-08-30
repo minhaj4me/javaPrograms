5915c9c835deeb050c2d7022264523d201bf4407
minhaj4me <minhajhaider007@gmail.com>
2022-08-30 22:51:03 +0530

    prog

---
+++ InsertElementArray.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class InsertElementArray {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        System.out
+                .println("Array is of 10 size, so make sure you enter less than size so that you can insert element ");
+        System.out.println("Enter the number of elements");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter the index at whilch you want to insert ");
+        int index = sc.nextInt();
+        System.out.println("Enter the value which you want to insert at " + index + " postion");
+        int element = sc.nextInt();
+        // if (index <= n) {
+        for (int i = n; i < index; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[index] = element;
+        // }
+        // else {
+        // arr[index] = element;
+        // }
+        for (int i : arr) {
+            System.out.println(i);
+        }
+    }
+}
