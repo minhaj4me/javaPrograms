5e9b2349ec83488c8f326b732433a1db19d68735
minhaj4me <minhajhaider007@gmail.com>
2022-09-20 20:30:12 +0530

    prog

---
+++ practiceIdentifier.java
@@ -0,0 +1,14 @@
+class practiceIdentifier{
+    public static void main(String[] args) {
+
+int String=20;
+int Runnable=20;
+char ch='\u0062';
+char n='\n';
+System.out.println(n);
+System.out.println(ch);
+System.out.println(String);
+System.out.println(Runnable);
+
+    }
+}
