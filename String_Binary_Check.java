f2c53a55755234e7c68e30415f2f922040c4bc24
minhaj4me <minhajhaider007@gmail.com>
2022-09-23 23:43:52 +0530

    String_Binary_Check

---
+++ String_Binary_Check.java
@@ -0,0 +1,11 @@
+class String_Binary_Check {
+    public static void main(String[] args) {
+        String str = "101";
+        String arr[] = str.split("|");
+        for (int i = 0; i<arr.length; i++) {
+          
+
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
