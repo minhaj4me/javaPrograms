5389840a3e2dc8f36dbb843c2e6a98ec18dd81e1
minhaj4me <minhajhaider007@gmail.com>
2022-10-03 21:42:17 +0530

    Assignment_23_aug/.project

---
+++ MyArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * MyArrayList
+ */
+public class MyArrayList {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> a=new ArrayList<>();
+        a.add(5);
+    }
+}
