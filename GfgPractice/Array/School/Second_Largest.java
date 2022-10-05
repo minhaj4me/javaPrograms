08f4644a9686f9560b4597bbc83a0ba2ac9089aa
minhaj4me <minhajhaider007@gmail.com>
2022-10-05 17:30:15 +0530

    gfg

--- GfgPractice/Array/School/Second_Largest.java
+++ GfgPractice/Array/School/Second_Largest.java
@@ -1,3 +1,29 @@
 public class Second_Largest {
-    
+
+    int print2largest(int arr[], int n) {
+        // code here
+        int firstElement = -1;
+        int secondElement = -1;
+
+        if (n == 0 || n == 1)
+            return -1;
+
+        int i = 0;
+        while (i < n) {
+            if (arr[i] > firstElement) {
+                secondElement = firstElement;
+                firstElement = arr[i];
+            } else {
+                if (arr[i] > secondElement && arr[i] != firstElement) {
+                    secondElement = arr[i];
+                }
+
+            }
+            i++;
+        }
+        return secondElement;
+        // Arrays.sort(arr);
+        // return arr[arr.length-2];
+
+    }
 }
