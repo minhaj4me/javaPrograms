b15931906a816a283664fc83c34abcb99737d55a
minhaj4me <minhajhaider007@gmail.com>
2022-10-17 20:48:11 +0530

    Hollow_Rectangle

---
+++ AAC/Patterns/Hollow_Rectangle.java
@@ -0,0 +1,21 @@
+/**
+ * Hollow_Rectangle
+ */
+public class Hollow_Rectangle {
+
+    public static void main(String[] args) {
+        int row = 6;
+        int col = 6;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (i == 1 || j == 1 || i == row || j == col) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+}
