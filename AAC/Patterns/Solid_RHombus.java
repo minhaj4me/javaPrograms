f5ce4fe4c4214a91a5167f73ad924f0eccd8a07c
minhaj4me <minhajhaider007@gmail.com>
2022-10-21 17:40:50 +0530

    Solid_RHombus

---
+++ AAC/Patterns/Solid_RHombus.java
@@ -0,0 +1,17 @@
+public class Solid_RHombus {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int sp = 1; sp <= n - i; sp++) {
+                System.out.print(" ");
+            }
+            for (int st = 1; st <= n; st++) {
+                System.out.print("*" +" ");
+            }
+            System.out.println();
+        }
+    }
+}
