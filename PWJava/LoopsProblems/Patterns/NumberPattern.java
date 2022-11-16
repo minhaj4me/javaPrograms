a2758874ac7bde8fca46d82265d560d0176f6ec5
minhaj4me <minhajhaider007@gmail.com>
2022-11-16 15:10:26 +0530

    NumberPattern

---
+++ PWJava/LoopsProblems/Patterns/NumberPattern.java
@@ -0,0 +1,12 @@
+public class NumberPattern {
+public static void main(String[] args) {
+    int row=6;
+    int col=6;
+    for (int i = 1; i <=row; i++) {
+        for (int j = 1; j <=col; j++) {
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
+}
+}
