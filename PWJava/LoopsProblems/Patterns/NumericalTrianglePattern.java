0e82a4c8d539df12497628c28a96cfa9418f7da5
minhaj4me <minhajhaider007@gmail.com>
2022-11-20 17:36:45 +0530

    NumericalTrianglePattern

---
+++ PWJava/LoopsProblems/Patterns/NumericalTrianglePattern.java
@@ -0,0 +1,11 @@
+public class NumericalTrianglePattern {
+    public static void main(String[] args) {
+        int row=4;
+        for (int i = 1; i <=row; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
