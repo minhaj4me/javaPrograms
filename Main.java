1c3c703c9c1c0ffb6d04986d96201c1de2f17e1d
Minhaj Haider <63444276+minhaj4me@users.noreply.github.com>
2023-09-19 13:42:51 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,55 +1,23 @@
-class A {
-    int i = 10;
-    // System.out.println(i);
-}
-
-class B extends A {
-    int i = 20;
-
-    // System.out.println(i);
-}
-
-class C extends B {
-    int i = 30;
-
-    public void m1() {
-        System.out.println(super.i);
-    }
-
-    A a = new A();
-
-    public void m2() {
-        System.out.println(a.i);
-    }
-
-    // System.out.println(i);
-}
-
-class H{
-    H(){
-        System.out.println("tt");
-    }
-    H(int i){
-System.out.println("lll");
-    }
-}
-class G extends H{
-G(){
-    super(6);
-    System.out.println("ggg");
-}
-}
-
-/**
- * Main
- */
-public class Main {
-
-    public static void main(String[] args) {
-        C c = new C();
-        System.out.println(c.i);
-        c.m1();
-        c.m2();
-        new G();
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] num = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 
+            21, 22, 23, 24, 25,  27, 28, 29, 30, 
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
+            41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 
+            51, 52, 53, 54, 55,56, 57, 58, 59, 60, 
+            61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 
+            71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 
+            81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 
+            91, 92, 93, 94, 95, 96, 97, 98, 99, 100};
+		int check=1;
+		for (int i =0;i<num.length ;i++) {
+		        if (num[i]!=check){
+		        System.out.print("Number Missing is "+check);
+		        break;
+		        }
+		        check++;
+		}
+	}
 }
