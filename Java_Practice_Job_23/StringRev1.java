d4a560e9a5606cfa9884000800a91417b90885ac
minhaj4me <minhajhaider007@gmail.com>
2023-09-22 23:16:19 +0530

    PracticeForJob2023

---
+++ Java_Practice_Job_23/StringRev1.java
@@ -0,0 +1,12 @@
+
+// charAt
+public class StringRev1 {
+
+    public static void main(String[] args) {
+        String str= "hello";
+        for (int i = str.length()-1; i >=0; i--) {
+
+            System.out.print(str.charAt(i));
+        }
+    }
+}
