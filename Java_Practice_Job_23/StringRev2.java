d695814270ab037a10a0dd4922aa8c1bee2161c7
minhaj4me <minhajhaider007@gmail.com>
2023-09-23 13:06:18 +0530

    String Reverse 2

---
+++ Java_Practice_Job_23/StringRev2.java
@@ -0,0 +1,11 @@
+// str.toCharArray
+public class StringRev2 {
+ public static void main(String[] args) {
+    String str= "hello";
+
+    char ch[]=str.toCharArray(); 
+    for (int i = ch.length-1; i >=0; i--) {
+        System.out.print(ch[i]);
+    }
+}   
+}
