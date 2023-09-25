0079c139d54d321232dd8d03b46f412eeae641e7
minhaj4me <minhajhaider007@gmail.com>
2023-09-25 11:41:25 +0530

    String Reverse 3

---
+++ Java_Practice_Job_23/StringRev3.java
@@ -0,0 +1,10 @@
+// charAt
+public class StringRev3 {
+
+    public static void main(String[] args) {
+        String str = "hello";
+        StringBuffer sb=new StringBuffer(str);
+
+            System.out.print(sb.reverse());
+        }
+    }
