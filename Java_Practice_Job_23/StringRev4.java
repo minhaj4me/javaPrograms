9f96a1dee1a14643997fb029364dca3467e8c356
minhaj4me <minhajhaider007@gmail.com>
2023-09-26 15:28:17 +0530

    String Reverse another way

---
+++ Java_Practice_Job_23/StringRev4.java
@@ -0,0 +1,10 @@
+// charAt
+public class StringRev4 {
+
+    public static void main(String[] args) {
+        String str = "hello";
+        StringBuilder sb = new StringBuilder(str);
+
+        System.out.print(sb.reverse());
+    }
+}
